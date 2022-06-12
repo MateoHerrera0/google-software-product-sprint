a080099f432b5eb51f0aaa1f712e8fb57e9eccda
Mateo <mateo.herrera.prof@gmail.com>
2022-06-12 01:20:52 +0000

    Add servlet string list return

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,19 +15,26 @@
 /**
  * Adds a random quote to the page.
  */
-function addQuote() {
-  const quotes = [
-    '“Do You Like Scary Movies?” - Scream', 
-    '“Do You Want To Play A Game?” - Saw', 
-    '“It Rubs The Lotion On Its Skin Or Else It Gets The Hose Again.” - The Silence Of The Lambs',
-    '"Groovy" - Evil Dead II', '“Nobody trusts anybody now…and we\'re all very tired.” - The Thing', 
-    '“It\'s Halloween; I guess everyone\'s entitled to one good scare.” - Halloween'
-  ];
+async function addQuote() {
+  // const quotes = [
+  //   '“Do You Like Scary Movies?” - Scream', 
+  //   '“Do You Want To Play A Game?” - Saw', 
+  //   '“It Rubs The Lotion On Its Skin Or Else It Gets The Hose Again.” - The Silence Of The Lambs',
+  //   '"Groovy" - Evil Dead II', '“Nobody trusts anybody now…and we\'re all very tired.” - The Thing', 
+  //   '“It\'s Halloween; I guess everyone\'s entitled to one good scare.” - Halloween'
+  // ];
 
+  const responseFromServer = await fetch('/quote');
+  // The json() function returns an object that contains fields that we can
+  // reference to create HTML.
+  const quotes = await responseFromServer.json();
+  console.log(quotes);
+  
   // Pick a random quote.
   const chosenQuote = quotes[Math.floor(Math.random() * quotes.length)];
-
   // Add it to the page.
   const quoteContainer = document.getElementById('quote');
   quoteContainer.innerText = chosenQuote;
 }
+
+
